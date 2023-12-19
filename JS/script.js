376b746b57f9bd58a39028f3efc224d61cb2d75a
Bernardo Dartora <bernardintd@gmail.com>
2023-12-19 17:01:28 -0300

    pre-product

--- JS/script.js
+++ JS/script.js
@@ -1,134 +1,128 @@
-const characterList = document.getElementById('character-list')
-        const paginationContainer = document.getElementById('pagination')
-        const apiUrl = 'https://rickandmortyapi.com/api/character'
-        let currentPage = 1
-        let totalPages = 1
 
-        async function getCharacters(page = 1) {
-            const response = await fetch(`${apiUrl}?page=${page}`)
-            const data = await response.json()
-        
-            const charactersWithEpisodes = await Promise.all(
-                data.results.map(async character => {
-                    const episodesPromises = character.episode.map(async episodeUrl => {
-                        const episodeResponse = await fetch(episodeUrl)
-                        const episodeData = await episodeResponse.json()
-                        return episodeData
+const apiUrl = 'https://rickandmortyapi.com/api/character';
+let currentPage = 1;
+let totalPages = 1;
+
+async function getCharacters(page = 1) {
+    try {
+        const response = await axios.get(`${apiUrl}?page=${page}`);
+        const charactersWithEpisodes = await Promise.all(
+            response.data.results.map(async character => {
+                const episodes = await Promise.all(
+                    character.episode.map(async episodeUrl => {
+                        const episodeResponse = await axios.get(episodeUrl);
+                        return episodeResponse.data;
                     })
-        
-                    const episodes = await Promise.all(episodesPromises)
-        
-                    // Encontrar o último episódio
-                    const lastEpisode = episodes.reduce((latest, current) => {
-                        const currentEpisodeDate = new Date(current.air_date)
-                        const latestEpisodeDate = new Date(latest.air_date)
-
-                        return currentEpisodeDate > latestEpisodeDate ? current : latest
-                    })
-        
-                    character.lastEpisode = lastEpisode.name
-                    return character
-                })
-            )
-        
-            totalPages = data.info.pages
-            return { ...data, results: charactersWithEpisodes }
-        }
-        
-        function displayCharacters(characters) {
-            characterList.innerHTML = ''
-        
-            characters.results.forEach(character => {
-                const characterCard = document.createElement('div')
-                characterCard.classList.add('character-card')
-        
-                // Adiciona a lógica para definir a cor da indicação de status
-                let statusColor = ''
-                switch (character.status) {
-                    case 'Alive':
-                        statusColor = 'green'
-                        break
-                    case 'Dead':
-                        statusColor = 'red'
-                        break
-                    default:
-                        statusColor = 'gray'
-                }
-                characterCard.innerHTML = `
-                <img src="${character.image}" class="character-image" alt="${character.name}">
-                <div class="character-description">
-                    <p>${character.id}</p>
-                    <h2>${character.name}</h2>
-                    <div class="status">
-                        <div class="status-indicator" style="background-color: ${statusColor}"></div>
-                        <span>${character.status} - ${character.species}</span>
-                    </div>
-
-                    <p>Última localização conhecida: ${character.location.name}</p>
-                    <p>Visto pela última vez em: ${character.lastEpisode}</p>
-                </div>
-                `
-
-                characterList.appendChild(characterCard)
+                );
+                character.lastEpisode = episodes.reduce((latest, current) => 
+                    new Date(current.air_date) > new Date(latest.air_date) ? current : latest
+                ).name;
+                return character;
             })
-        }
+        );
+        totalPages = response.data.info.pages;
+        return { ...response.data, results: charactersWithEpisodes };
+    } catch (error) {
+        console.error('Error fetching characters:', error);
+    }
+}
 
-        function displayPagination() {
-            paginationContainer.innerHTML = ''
-            const buttonAnteriorHTML = currentPage > 1 ? `<button id="btn-anterior" onclick=changePage(${currentPage - 1})> ${currentPage - 1} </button>` : ''
-            const buttonAtualHTML = `<button style="background-color: gray " class="current-page">${currentPage}</button>`
-            const buttonPosteriorHTML = currentPage < totalPages ? `<button id="btn-posterior" onclick=changePage(${currentPage + 1})> ${currentPage + 1} </button>` : ''
-            const buttonFinalHTML = currentPage + 1 === totalPages || currentPage === totalPages ? '' : `
-                <span>...</span>
-                <button id="btn-posterior" onclick=changePage(${totalPages})> ${totalPages} </button>
-            `
-            paginationContainer.innerHTML = `
-                ${buttonAnteriorHTML}
-                ${buttonAtualHTML}
-                ${buttonPosteriorHTML}
-                ${buttonFinalHTML}
-            `
+function displayCharacters(characters) {
+    const characterList = document.getElementById('character-list');
+    characterList.innerHTML = characters.results.map(character => `
+        <div class="character-card">
+            <img src="${character.image}" class="character-image" alt="${character.name}">
+            <div class="character-description">
+                <p>${character.id}</p>
+                <h2>${character.name}</h2>
+                <div class="status">
+                    <div class="status-indicator" style="background-color: ${getStatusColor(character.status)}"></div>
+                    <span>${character.status} - ${character.species}</span>
+                </div>
+                <p>Última localização conhecida: ${character.location.name}</p>
+                <p>Visto pela última vez em: ${character.lastEpisode}</p>
+            </div>
+        </div>
+    `).join('');
+}
+
+function getStatusColor(status) {
+    switch (status) {
+        case 'Alive':
+            return 'green';
+        case 'Dead':
+            return 'red';
+        default:
+            return 'gray';
+    }
+}
+
+function displayPagination() {
+    const paginationContainer = document.getElementById('pagination');
+    const buttonAnteriorHTML = currentPage > 1 ? `<button onclick="changePage(${currentPage - 1})">${currentPage - 1}</button>` : '';
+    const buttonAtualHTML = `<button style="background-color: gray" class="current-page">${currentPage}</button>`;
+    const buttonPosteriorHTML = currentPage < totalPages ? `<button onclick="changePage(${currentPage + 1})">${currentPage + 1}</button>` : '';
+    const buttonFinalHTML = currentPage + 1 === totalPages || currentPage === totalPages ? '' : `
+        <span>...</span>
+        <button onclick="changePage(${totalPages})">${totalPages}</button>
+    `;
+    paginationContainer.innerHTML = `${buttonAnteriorHTML}${buttonAtualHTML}${buttonPosteriorHTML}${buttonFinalHTML}`;
+}
+
+async function fetchAndDisplayCharacters(page) {
+    const charactersData = await getCharacters(page);
+    currentPage = page;
+    displayCharacters(charactersData);
+    displayPagination();
+    fetchApiInfo();
+}
+
+async function searchCharacters() {
+    const searchTerm = document.getElementById('search-input').value.trim();
+    if (searchTerm === '') {
+        fetchAndDisplayCharacters(1);
+    } else {
+        try {
+            const response = await axios.get(`${apiUrl}?name=${searchTerm}`);
+            const searchData = { ...response.data, results: response.data.results.map(character => ({ ...character, lastEpisode: '' })) };
+            currentPage = 1;
+            totalPages = 1;
+            displayCharacters(searchData);
+            displayPagination();
+        } catch (error) {
+            console.error('Error searching characters:', error);
         }
-        async function fetchAndDisplayCharacters(page) {
-            const charactersData = await getCharacters(page)
-            currentPage = page
-            displayCharacters(charactersData)
-            displayPagination()
-            fetchApiInfo()
-        }
-        // Carregar personagens da primeira página ao carregar a página
-        fetchAndDisplayCharacters(1)
-        const searchInput = document.getElementById('search-input')
-        async function searchCharacters() {
-            const searchTerm = searchInput.value.trim()
-            if (searchTerm === '') {
-                fetchAndDisplayCharacters(1)
-            } else {
-                const response = await fetch(`${apiUrl}?name=${searchTerm}`)
-                const searchData = await response.json()
-                displayCharacters(searchData)
-            }
-        }
-        async function fetchApiInfo() {
-            const charactersResponse = await fetch('https://rickandmortyapi.com/api/character')
-            const charactersData = await charactersResponse.json()
-            const locationsResponse = await fetch('https://rickandmortyapi.com/api/location')
-            const locationsData = await locationsResponse.json()
-            const episodesResponse = await fetch('https://rickandmortyapi.com/api/episode')
-            const episodesData = await episodesResponse.json()
-            const apiInfoContainer = document.getElementById('api-info')
-            apiInfoContainer.innerHTML = `
-                <p>Total de Personagens: ${charactersData.info.count}</p>
-                <p>Total de Localizações: ${locationsData.info.count}</p>
-                <p>Total de Episódios: ${episodesData.info.count}</p>
-                <p>Desenvolvido por BernardoDartora em 2023</p>
-            `
-        }
-        function changePage(newPage) {
-            if (newPage >= 1 && newPage <= totalPages) {
-                fetchAndDisplayCharacters(newPage)
-            }
-        }
-        function reloadPage() {
-            location.reload()
-        }
+    }
+}
+
+async function fetchApiInfo() {
+    try {
+        const [charactersData, locationsData, episodesData] = await Promise.all([
+            axios.get('https://rickandmortyapi.com/api/character'),
+            axios.get('https://rickandmortyapi.com/api/location'),
+            axios.get('https://rickandmortyapi.com/api/episode')
+        ]);
+        const apiInfoContainer = document.getElementById('api-info');
+        apiInfoContainer.innerHTML = `
+            <p>Total de Personagens: ${charactersData.data.info.count}</p>
+            <p>Total de Localizações: ${locationsData.data.info.count}</p>
+            <p>Total de Episódios: ${episodesData.data.info.count}</p>
+            <p>Desenvolvido por BernardoDartora em 2023</p>
+        `;
+    } catch (error) {
+        console.error('Error fetching API info:', error);
+    }
+}
+
+function changePage(newPage) {
+    if (newPage >= 1 && newPage <= totalPages) {
+        fetchAndDisplayCharacters(newPage);
+    }
+}
+
+function reloadPage() {
+    location.reload();
+}
+
+// Load characters from the first page when the page is loaded
+fetchAndDisplayCharacters(1);
